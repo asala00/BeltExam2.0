1964393150bd9a4e2e750c58d541bfd5e4e28c5a
Asala <109462856+asala00@users.noreply.github.com>
2022-09-01 14:33:08 +0300

    general edits.

--- Assets/scripts/ui/titlescreen.cs
+++ Assets/scripts/ui/titlescreen.cs
@@ -1,16 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TitleScreen : MonoBehaviour
 {
+    public MoveIn3d PlayerMovemntScript;
     [SerializeField] private GameObject titleScreenCanvas;
     [SerializeField] private GameObject playerHUD;
     [SerializeField] private GameObject refundCanvas;
     public SoundManager SM;
     public AudioSource BackGroundMusic;
+
+    private void Awake()
+    {
+        PlayerMovemntScript.enabled = false;
+    }
+
     public void StartG()
     {
+        PlayerMovemntScript.enabled = true;
         titleScreenCanvas.SetActive(false);
         Cursor.lockState = CursorLockMode.Locked;
         playerHUD.SetActive(true);
